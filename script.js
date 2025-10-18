597e82b55638c9333d4d8485b6b899ca8ecee64a
Jared Recomendable <41513577+JDCRecomendable@users.noreply.github.com>
2025-10-18 13:54:35 +1300

    fix: Stay on the same tab when searching with DDG

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function constructUrl(rawQuery) {
-    return "https://duckduckgo.com/?q=" + rawQuery;
+    // encode the query so characters/spaces don't break the URL
+    return "https://duckduckgo.com/?q=" + encodeURIComponent(rawQuery);
 }
 
 function processSearch() {
     var rawQuery = document.getElementById("search-bar").value;
     url = constructUrl(rawQuery);
-    window.open(url);
+    window.location.assign(url);
 }
